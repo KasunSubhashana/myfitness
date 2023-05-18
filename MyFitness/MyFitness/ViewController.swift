91a9af62fde04121e55adaf8d468ebd29ebbf5d1
Kasun Subhashana <kasunsubhashana24@gmail.com>
2023-05-19 02:37:18 +0530

    create project

---
+++ MyFitness/MyFitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFitness
+//
+//  Created by Kasun Subhashana on 19/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
